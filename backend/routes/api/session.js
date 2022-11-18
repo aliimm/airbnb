3209a3d7562d56c9ffe1a64274491c27132f83bb
alim <alimshussain@gmail.com>
2022-11-18 08:52:46 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,7 +8,53 @@ const router = express.Router();
 
 // backend/routes/api/session.js
 // ...
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// ...
 
+// backend/routes/api/session.js
+// ...
+// backend/routes/api/session.js
+// ...
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+  // backend/routes/api/session.js
+// ...
+
+// Log in
+router.post(
+    '/',
+    validateLogin,
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+
+      const user = await User.login({ credential, password });
+
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user: user
+      });
+    }
+  );
 // Log in
 router.post(
     '/',
@@ -63,4 +109,7 @@ router.get(
   );
 
   // ...
+
+
+
 module.exports = router;
