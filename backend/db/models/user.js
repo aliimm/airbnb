0f3b93b07c78cd5608a9d9076be15f0d1f7a5dd3
alim <alimshussain@gmail.com>
2022-11-21 11:08:45 -0800

    added back allownull

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -67,9 +67,11 @@ module.exports = (sequelize, DataTypes) => {
       },
       firstName: {
         type: DataTypes.STRING,
+        allowNull: false
       },
       lastName: {
         type: DataTypes.STRING,
+        allowNull: false
       },
       email: {
         type: DataTypes.STRING,
