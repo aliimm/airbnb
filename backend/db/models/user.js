21f47b91631671eff471f1b4922b798cbd915b92
alim <alimshussain@gmail.com>
2022-11-22 10:17:02 -0800

    db update

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static associate(models) {
+      User.hasMany(models.Spot, {foreignKey: 'ownerId'})
       // define association here
     }
 
