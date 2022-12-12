75f3dbba062b24ab487823887dd3a4e393d65a0c
alim <alimshussain@gmail.com>
2022-12-12 00:45:24 -0800

    backend changes to index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,32 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
-// backend/routes/index.js
-// ...
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
-// ...
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-// ...
-
-// backend/routes/index.js
-// ... after `router.use('/api', apiRouter);`
 
 // Static routes
 // Serve React build files in production
@@ -35,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -46,27 +23,18 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
-// backend/routes/index.js
-// ...
-
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
-
-
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
-
 
 module.exports = router;
