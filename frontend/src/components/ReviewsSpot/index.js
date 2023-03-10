d0b62f915417ac19c5b849e9bacbf98970dc24d3
alim <alimshussain@gmail.com>
2023-03-09 18:01:30 -0800

    stlying details spots

--- frontend/src/components/ReviewsSpot/index.js
+++ frontend/src/components/ReviewsSpot/index.js
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {deleteReview} from '../../store/review'
-import {  useHistory } from 'react-router-dom';
+import { deleteReview } from '../../store/review'
+import { useHistory } from 'react-router-dom';
+import './reviewspot.css'
 
 
 function AllReviewsForSpot() {
@@ -15,26 +16,30 @@ function AllReviewsForSpot() {
     const reviewSelector = useSelector(state => {
         return state.reviews.reviewList
     });
+    const spotSelector = useSelector(state => {
+        return state.spots.oneSpot
+    });
+    console.log(spotSelector)
 
     const reviewArray = Object?.values(reviewSelector)
 
     const reviewDelete = async (reviewId) => {
         return dispatch(deleteReview(reviewId))
-        .then(() => history.push('/'))
-        .catch(
-            async (res) => {
-                if (!res.ok) {
-                    const data = await res.json();
-                    if (data.message.includes('Authentication required')) setErrors(['Need to be signed in to make or delete a review'])
-                    else if (data && data.errors) setErrors(data.errors);
-                    else if (data && data.message) setErrors([data.message])
+            .then(() => history.push('/'))
+            .catch(
+                async (res) => {
+                    if (!res.ok) {
+                        const data = await res.json();
+                        if (data.message.includes('Authentication required')) setErrors(['Need to be signed in to make or delete a review'])
+                        else if (data && data.errors) setErrors(data.errors);
+                        else if (data && data.message) setErrors([data.message])
+
+                    }
+
+
 
                 }
-
-
-
-            }
-        );
+            );
 
     }
 
@@ -46,21 +51,39 @@ function AllReviewsForSpot() {
             <p>
                 {errors.map((error, idx) => <li key={idx}>{error}</li>)}
             </p>
-            <h1>Reviews</h1>
-            <nav className='container'>
+
+            <div className='title-reviews'><i class="fa-solid fa-star"></i>
+                {reviewArray.length === 0 ?
+
+                    <> No Reviews Yet</> :
+                    <>{spotSelector.avgStarRating.toFixed(2)} · {reviewArray.length} reviews</>
+
+                }
+            </div>
+
+            <nav className='container-reviews'>
                 {reviewArray?.length ? reviewArray?.map(element => (
                     <div className='reviewCard' key={element.id}>
-                        <h3>{element.User.firstName}: {element.review} </h3>
-                        <h4>{element.stars} <i class="fa-solid fa-star"></i></h4>
-                        <button onClick= {() => reviewDelete(element.id)} >Delete Review</button>
+                        <div className='review-card-img-and-name'>
+                            <div><img className='profileimg-reviewcard' src={element.User.profileimg}></img></div>
+                            <div className='user-name-review'>
+                                <div>{element.User.firstName}</div>
+                                <div>{element.createdAt}</div>
+                            </div>
+                        </div>
+                        <div className='review-message'>{element.review}</div>
+
+                        {/* <h4>{element.stars} <i class="fa-solid fa-star"></i></h4> */}
+                        {/* < button onClick={() => reviewDelete(element.id)} >Delete Review</button> */}
 
                     </div>
-                )) : <div>No Reviews Yet</div>}
+                )) : <div>No Reviews Yet</div>
+                }
 
 
-            </nav>
+            </nav >
 
-        </nav>
+        </nav >
 
 
     )
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -45,8 +45,8 @@ const SpotDetails = () => {
         <nav className='container-detail-page'>
             <div className='detailsDiv'>
                 <div className='imgContainer'>
-                <p className='title-details-spots'>{spotSelector.name}</p>
-                <div className='infobar'><i className="fa-sharp fa-solid fa-star"></i> {+spotSelector.avgStarRating ? spotSelector.avgStarRating.toFixed(2) : <>No Reviews Yet</>} · {spotSelector.numReviews} reviews  · {spotSelector.city}, {spotSelector.state}, {spotSelector.country}</div>
+                    <p className='title-details-spots'>{spotSelector.name}</p>
+                    <div className='infobar'><i className="fa-sharp fa-solid fa-star"></i> {+spotSelector.avgStarRating ? spotSelector.avgStarRating.toFixed(2) : <>No Reviews Yet</>} · {spotSelector.numReviews} reviews  · {spotSelector.city}, {spotSelector.state}, {spotSelector.country}</div>
                     {spotSelector.SpotImages.map(x => (<div><img src={x.url} className='image' /></div>))}
                     {(spotSelector.ownerId === currentUser) &&
                         <div>
@@ -58,24 +58,47 @@ const SpotDetails = () => {
                     <div className='container-booking-details'>
                         <div className='left-side-details'>
                             <div className='hostName'>
-                                <div className='entire-host-message'>Entire guest BNB hosted by {spotSelector.Owner.firstName}</div>
+                                <div className='entire-host-message'>Entire place hosted by {spotSelector.Owner.firstName}</div>
                                 <div className='pfp-hostname-div'><img className='owner-pfp-spot-details' src={spotSelector.Owner.profileimg}></img></div>
                             </div>
 
                             <div className='superhost-div'>
-                            <div><i class="fa-solid fa-medal fa-xl"></i></div>
-                            <div className='host-div-right-side'>
-                                <div className='super-host-title'>{spotSelector.Owner.firstName} is a Superhost</div>
-                                <div className='super-host-message-desc'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests</div>
+                                <div><i class="fa-solid fa-medal fa-xl"></i></div>
+                                <div className='host-div-right-side'>
+                                    <div className='super-host-title'>{spotSelector.Owner.firstName} is a Superhost</div>
+                                    <div className='super-host-message-desc'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests</div>
+                                </div>
                             </div>
 
+                            <div className='amenities-container'>
+                                <div className='single-amenity-container'>
+                                    <div className='amenity-icon'><i class="fa-solid fa-door-open fa-xl"></i></div>
+                                    <div className='amenity-text'>
+                                        <div className='amenities-title'>Self check-in</div>
+                                        <div className='amenities-desc'>Check yourself in with the lockbox.</div>
+                                    </div>
+                                </div>
 
+                                <div className='single-amenity-container'>
+                                    <div className='amenity-icon'><i class="fa-solid fa-location-dot fa-xl"></i></div>
+                                    <div className='amenity-text'>
+                                        <div className='amenities-title'>Great location</div>
+                                        <div className='amenities-desc'>100% of recent guests gave the location a 5-star rating.</div>
+                                    </div>
+                                </div>
+
+                                <div className='single-amenity-container'>
+                                    <div className='amenity-icon'><i class="fa-solid fa-key fa-xl"></i></div>
+                                    <div className='amenity-text'>
+                                        <div className='amenities-title'>Great check-in experience</div>
+                                        <div className='amenities-desc'>100% of recent guests gave the check-in process a 5-star rating.</div>
+                                    </div>
+                                </div>
                             </div>
 
 
                             <div className='descriptionBlock'>
-                                <h1>Description:</h1>
-                                <p>{spotSelector.description}</p>
+                                <p className='desc-one-spot'>{spotSelector.description}</p>
                             </div>
 
 
@@ -87,14 +110,17 @@ const SpotDetails = () => {
                                 </div>
                             }
 
-                            <div>
-                                <AllReviewsForSpot />
-                            </div>
                         </div>
                         <div className='specialDiv'>
                             <p className='priceinspecial'>${spotSelector.price} night</p>
+
                         </div>
                     </div>
+
+                    <div className='reviews-container-details'>
+                        <AllReviewsForSpot />
+                    </div>
+
                 </div>
             </div>
         </nav >
